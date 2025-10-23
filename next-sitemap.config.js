97bf974125b6463b697c5f3e78c030d303f6d7f7
its-pz-mir <usmanshouket256@gmail.com>
2025-10-23 17:27:22 +0500

    sitemapp added in app directory

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://usmanshouket.tech', // <-- Replace with your actual domain
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 7000,
-}
